2a1835499061b1195e9d573abe468b06527354fc
cookies2002 <cookies2002boy@gmail.com>
2025-05-01 19:39:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,20 +3,18 @@ from plugins.config import Config
 from pyrogram import Client
 
 if __name__ == "__main__":
-
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
 
     plugins = dict(root="plugins")
 
     app = Client(
-        session_name=Config.SESSION_STR,  # Use your user session
+        Config.SESSION_STR,  # <-- This is your session string
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
-        sleep_threshold=300,
         plugins=plugins
     )
 
-    print("🎊 USER BOT IS ALIVE 🎊  • Support @NT_BOTS_SUPPORT")
+    print("🎊 USERBOT ONLINE (4GB upload supported) 🎊  • Support @NT_BOTS_SUPPORT")
     app.run()
     
