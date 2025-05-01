aa3578afcd5443959dbcb366eeea96be628f8ffe
cookies2002 <cookies2002boy@gmail.com>
2025-05-01 20:37:12 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -1,30 +1,30 @@
 import os
-from os import environ, getenv
 import logging
 
 logging.basicConfig(
     format='%(name)s - %(levelname)s - %(message)s',
-    handlers=[logging.FileHandler('log.txt'),
-              logging.StreamHandler()],
+    handlers=[
+        logging.FileHandler('log.txt'),
+        logging.StreamHandler()
+    ],
     level=logging.INFO
 )
 
 class Config(object):
-    
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7917736285:AAEYMg-lGv58V079brKRX3ZEU8ONB7ETQyw")
     API_ID = int(os.environ.get("API_ID", "25934170"))
     API_HASH = os.environ.get("API_HASH", "d199295026b1e44ec3c72ef53f806a1f")
-    
+
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    MAX_FILE_SIZE = 4294967296
-    TG_MAX_FILE_SIZE = 4294967296
-    FREE_USER_MAX_FILE_SIZE = 4294967296
-    SESSION_STR = ""
-    FREE_USER_MAX_FILE_SIZE = 2194304000
+    MAX_FILE_SIZE = 4294967296  # 4GB
+    TG_MAX_FILE_SIZE = 4294967296  # 4GB
+    FREE_USER_MAX_FILE_SIZE = 4294967296  # 4GB
+
+    SESSION_STR = ""  # Leave empty for bot, use session string in userbot script
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-    
+
     OUO_IO_API_KEY = ""
     MAX_MESSAGE_LENGTH = 4096
     PROCESS_MAX_TIMEOUT = 3600
@@ -40,18 +40,14 @@ class Config(object):
     SESSION_NAME = "Synthex_Sx_Bot"
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002644970737")
 
-    TG_MIN_FILE_SIZE = 2194304000
+    TG_MIN_FILE_SIZE = 4294967296  # 4GB for userbot
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Synthex_Sx_Bot")
     ADL_BOT_RQ = {}
 
-    # Set False off else True
     TRUE_OR_FALSE = os.environ.get("TRUE_OR_FALSE", "").lower() == "true"
 
-    # Shortlink settings
-    SHORT_DOMAIN = environ.get("SHORT_DOMAIN", "")
-    SHORT_API = environ.get("SHORT_API", "")
+    SHORT_DOMAIN = os.environ.get("SHORT_DOMAIN", "")
+    SHORT_API = os.environ.get("SHORT_API", "")
 
-    # Verification video link
     VERIFICATION = os.environ.get("VERIFICATION", "")
-
     
