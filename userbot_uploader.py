726a5a3e90e686df67108189e8555c1fd20dd50e
cookies2002 <cookies2002boy@gmail.com>
2025-05-01 20:38:22 +0530

    Update userbot_uploader.py

--- userbot_uploader.py
+++ userbot_uploader.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 import os
 
 # 1. Paste your actual session string, API ID and hash here
-SESSION_STRING = "YOUR_SESSION_STRING"  # Replace with your session string
+SESSION_STRING = "BQGLuVoATGjDdJJADRCABfCrVMz98w4M0ZOy2HjMDG6Y59ikghbHxBj5TYn97hp9BmtFOfVGl6uBxWvd4jbN6T_scF02FlqYK-aI1tGEpKFRBKCXdhk8czby34wF5I6NUz0krKQcejaKOQhSnCZo70WD0VpcPA1CdeXa4cL4lY8eqdMjS205QJfnJ3z5Wa8bgbDIplpOAvcijFZ5reBgxvaYt9B_LWeFxEvtaWBo4lxtqnMyMRt3INP15EATbXDnXnSEdt50jeCO0C9c0uIyy7laYo8hb9emXgxT3uOJB3bYieGFpdkeGUs7voJ-REsZ-y2TWTPv869b1OpQZX_FEB0NGstATAAAAAHYECwWAA"  # Replace with your session string
 API_ID = 25934170  # Replace with your actual API ID
 API_HASH = "d199295026b1e44ec3c72ef53f806a1f"
 
